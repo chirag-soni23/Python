993594e5dce43c05f338c6bd29669f7052521e21
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-12 11:42:45 +0530

    Python

---
+++ Loops/10_solution.py
@@ -0,0 +1,11 @@
+#  Exponential Backoff
+import time
+wait_time = 1
+max_retries = 5
+attempts = 0
+
+while attempts < max_retries:
+    print("Attempt", attempts + 1, "- Wait time", wait_time)
+    time.sleep(wait_time)
+    wait_time *= 2
+    attempts += 1
