8b75d6c8b9f8cbd4130a3c749e756d8ade8718d8
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-12 11:28:44 +0530

    Python

---
+++ Loops/09_solution.py
@@ -0,0 +1,8 @@
+# List Uniqueness Checker
+items = ["apple", "banana", "orange", "apple", "mango"]
+unique_item = set()
+for item in items:
+       if item in unique_item:
+              print("Duplicate :", item)
+              break
+       unique_item.add(item)
