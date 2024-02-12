0b21d3ae04404289f4ccc0ae79df4ee3b5b1e3bc
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-12 08:35:02 +0530

    Python

--- Loops/05_solution.py
+++ Loops/05_solution.py
@@ -1,1 +1,9 @@
 # Find the First Non-Repeated Character
+input_str = "teetery"
+
+for char in input_str:
+       print(char)
+       if input_str.count(char) == 1:
+              print("Char is ", char)
+              break
+
