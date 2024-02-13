a8905106f7cc931f50da6bac7ec96f803d0da368
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-13 20:26:18 +0530

    python

---
+++ functions/02_solution.py
@@ -0,0 +1,8 @@
+# 2. Function with Multiple Parameters
+# Problem: Create a function that takes two numbers as parameters and returns their sum.
+
+def add(numOne,numTwo):
+      return numOne + numTwo
+
+result = add(2,3)
+print(result)
