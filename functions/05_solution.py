f08baa2e328173ae6463a8d05473de058d6a996d
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-13 20:47:13 +0530

    python

---
+++ functions/05_solution.py
@@ -0,0 +1,7 @@
+# 5. Default Parameter Value
+# Problem: Write a function that greets a user. If no name is provided, it should greet with a default name.
+
+def greet(name="User"):
+       return "Hello, " + name + " !"
+
+print(greet())
