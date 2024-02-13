a7d58c4bbdad45ee1de67eacc268ded3dacdf872
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-13 20:23:34 +0530

    python

--- functions/01_solution.py
+++ functions/01_solution.py
@@ -1,4 +1,9 @@
 # 1. Basic Function Syntax
 # Problem: Write a function to calculate and return the square of a number.
 
+def square(number):
+       return number ** 2
+
+result = square(4)   
+print(result)    
 
