1168b8e3b32eafda1466f532222b78b058526308
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-13 20:29:23 +0530

    python

---
+++ functions/03_solution.py
@@ -0,0 +1,9 @@
+# 3. Polymorphism in Functions
+# Problem: Write a function multiply that multiplies two numbers, but can also accept and multiply strings.
+
+def multiply(p1,p2):
+       return p1 * p2
+
+print(multiply(2,4))
+print(multiply('a',4))
+print(multiply(2,'a'))
