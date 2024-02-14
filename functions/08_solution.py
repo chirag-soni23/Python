4eef5712eab31bddfe4747d7c90556bdb7b680c6
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-14 08:17:04 +0530

    python

--- functions/08_solution.py
+++ functions/08_solution.py
@@ -1,3 +1,12 @@
 # 8. Function with **kwargs
 # Problem: Create a function that accepts any number of keyword arguments and prints them in the format key: value.
 
+# def print_kwargs(name,skill):
+def print_kwargs(**kwargs):
+       for key,value in kwargs.items():
+              print(f"{key}: {value}")
+       # print("Name", name, "and Skill is", skill)
+
+
+print_kwargs(name="Chirag", skill="React")
+print_kwargs(name="Ravindra",skill="Python",age=19)
