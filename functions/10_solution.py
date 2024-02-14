e8495665585efa0320265be67483862592f915de
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-14 08:47:38 +0530

    python

--- functions/10_solution.py
+++ functions/10_solution.py
@@ -1,3 +1,11 @@
 # 10. Recursive Function
 # Problem: Create a recursive function to calculate the factorial of a number.
 
+def factorial(n):
+       if n == 0:
+              return 1
+       else:
+              return n * factorial(n-1)
+       
+       
+print(factorial(5))
