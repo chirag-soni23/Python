eb94c3ecb1021efe626d33a192b0002ba91009c3
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-15 08:10:44 +0530

    python

--- scopes/02_scope.py
+++ scopes/02_scope.py
@@ -1,3 +1,4 @@
+# Example 1
 def f1():
        x = 88
        def f2():
@@ -5,5 +6,16 @@ def f1():
        return f2
 result = f1()
 result()
+
+# Example 2  Closures
+def coder(num):
+       def actual(x):
+              return x ** num
+       return actual
+f = coder(2)
+g = coder(3)
+print(f(3))
+print(g(3))
+              
 
 
