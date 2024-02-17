7e5a346efb4ee7f3f52820a3ea68685f8c558f69
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-17 20:30:25 +0530

    python

---
+++ OOP/01_solution.py
@@ -0,0 +1,9 @@
+# 1. Basic Class and Object
+# Problem: Create a Car class with attributes like brand and model. Then create an instance of this class.
+
+class Car:
+       brand = None
+       model = None
+
+my_car = Car()
+print(my_car)
