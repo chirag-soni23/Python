19ddd97f4c6d74e5499f3b87baa982fd3151a0fd
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-17 20:49:45 +0530

    python

--- OOP/01_solution.py
+++ OOP/01_solution.py
@@ -5,7 +5,8 @@ class Car:
        def __init__(self, brand,model):
               self.brand = brand
               self.model = model
-
+# 2. Class Method and Self
+# Problem: Add a method to the Car class that displays the full name of the car (brand and model).
        def full_name(self):
               return f"{self.brand} {self.model}"
 
--- OOP/02_solution.py
+++
@@ -1,3 +0,0 @@
-# 2. Class Method and Self
-# Problem: Add a method to the Car class that displays the full name of the car (brand and model).
-
