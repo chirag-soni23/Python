6acc114a3224dd879ac11355d6ffb6f2740bbe0e
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-18 08:14:44 +0530

    python

--- OOP/01_solution.py
+++ OOP/01_solution.py
@@ -2,9 +2,11 @@
 # Problem: Create a Car class with attributes like brand and model. Then create an instance of this class.
 
 class Car:
+       total_car = 0
        def __init__(self, brand,model):
               self.__brand = brand
               self.model = model
+              Car.total_car += 1
 
 
        def get_brand(self):
@@ -13,6 +15,9 @@ class Car:
 # Problem: Add a method to the Car class that displays the full name of the car (brand and model).
        def full_name(self):
               return f"{self.brand} {self.model}"
+       
+       def fuel_type(self):
+              return "Petrol or Diesel"
 # 3. Inheritance
 # Problem: Create an ElectricCar class that inherits from the Car class and has an additional attribute battery_size.
 class ElectricCar(Car):
@@ -20,17 +25,27 @@ class ElectricCar(Car):
               super().__init__(brand,model)
               self.battery_size = battery_size
 
+       def fuel_type(self):
+              return "Electric charge"       
+
 my_tesla = ElectricCar("Tesla","Model S","85KWH")
-print(my_tesla.model)
-print(my_tesla.full_name())
-print(my_tesla.get_brand())
+# print(my_tesla.model)
+# print(my_tesla.full_name())
+# print(my_tesla.get_brand())
+print(my_tesla.fuel_type())
+
+safari = Car("Tata","Safari")
+safari2 = Car("Tata","Nexon")
+print(safari.fuel_type())
+print(safari.total_car)
+
 
 
 my_car = Car("Toyota","Vellfire 🔥")
-print(my_car.brand)
-print(my_car.model)
-print(my_car.full_name())
+# print(my_car.brand)
+# print(my_car.model)
+# print(my_car.full_name())
 
 my_new_car = Car("Toyota","Fortuner 😎")
-print(my_new_car.brand)
-print(my_new_car.model)
+# print(my_new_car.brand)
+# print(my_new_car.model)
