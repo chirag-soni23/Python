a4472919cf138a780b600491faedfeb4070d36a7
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-18 20:02:32 +0530

    python

---
+++ decorators/01_solution.py
@@ -0,0 +1,16 @@
+# Problem 1: Timing Function Execution
+# Problem: Write a decorator that measures the time a function takes to execute.
+import time
+def timer(func):
+       def wrapper(*args, **kwargs):
+           start = time.time()
+           result =  func(*args,**kwargs)
+           end = time.time()
+           print(f"{func.__name__} ran in {end-start} time")
+           return result
+       return wrapper
+@timer
+def example_function(n):
+      time.sleep(n)
+
+example_function(2)      
