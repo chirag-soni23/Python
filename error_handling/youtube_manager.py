41fa9e123de5d51c8edbfd891bf2c2036d6ce10e
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-19 08:16:57 +0530

    python

--- error_handling/youtube_manager.py
+++ error_handling/youtube_manager.py
@@ -1,5 +1,11 @@
+import json
+
 def load_data():
-    pass
+    try:
+        with open('youtube.txt','r') as file:
+           return  json.load(file)
+    except FileNotFoundError:
+        return []
 
 def list_all_videos(videos):
     pass
