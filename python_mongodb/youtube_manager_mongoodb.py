280053c683151a66eb889b897906a8be72e2f20a
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-21 15:08:14 +0530

    pythoCollection(Database(MongoClient(host=['ac-4z4zqi2-shard-00-01.oq8kfpe.mongodb.net:27017', 'ac-4z4zqi2-shard-00-02.oq8kfpe.mongodb.net:27017', 'ac-4z4zqi2-shard-00-00.oq8kfpe.mongodb.net:27017'], document_class=dict, tz_aware=False, connect=True, authsource='admin', replicaset='atlas-fedab9-shard-0', tls=True),

--- python_mongodb/youtube_manager_mongoodb.py
+++ python_mongodb/youtube_manager_mongoodb.py
@@ -1,8 +1,20 @@
 from pymongo import MongoClient
-client = MongoClient("mongodb+srv://youtubepy:ADgj703@#@cluster0.oq8kfpe.mongodb.net/")
-# Not a good idea to include id and password in code files
+from urllib.parse import quote_plus
+
+# Escape username and password
+username = "youtubepy"
+password = "ADgj703@#"
+escaped_username = quote_plus(username)
+escaped_password = quote_plus(password)
+
+# Construct the connection URI with escaped username and password
+uri = f"mongodb+srv://{escaped_username}:{escaped_password}@cluster0.oq8kfpe.mongodb.net/"
+
+# Connect to the MongoDB cluster
+client = MongoClient(uri)
+
+# Access the "ytmanager" database and the "videos" collection
 db = client["ytmanager"]
 video_collection = db['videos']
 
 print(video_collection)
-
