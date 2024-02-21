ad257bac526c1fc5096fc9735ac83946ad0c419a
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-21 15:44:37 +0530

    python

--- python_mongodb/youtube_manager_mongoodb.py
+++ python_mongodb/youtube_manager_mongoodb.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from urllib.parse import quote_plus
+from bson import ObjectId
 
 # Escape username and password
 username = "youtubepy"
@@ -11,10 +12,63 @@ escaped_password = quote_plus(password)
 uri = f"mongodb+srv://{escaped_username}:{escaped_password}@cluster0.oq8kfpe.mongodb.net/"
 
 # Connect to the MongoDB cluster
-client = MongoClient(uri)
+client = MongoClient(uri,tlsAllowInvalidCertificates = True)
 
 # Access the "ytmanager" database and the "videos" collection
+# print(client)
 db = client["ytmanager"]
 video_collection = db['videos']
 
 print(video_collection)
+
+def list_videos():
+   for video in video_collection.find():
+      print(f"Id: {video['_id']}, name: {video['name']}, time: {video['time']}")
+
+def add_video(name,time):
+   video_collection.insert_one({"name": name, "time": time})
+
+def update_video(video_id,newName,newTime):
+   video_collection.update_one({"__id": ObjectId(video_id)},{'$set': {"name": newName, "time": newTime}})
+
+def delete_video(video_id):
+   video_collection.delete_one({'__id':ObjectId(video_id)})
+
+def main():
+       while True:
+              print("\n Youtube manager App")
+              print("1. List all videos")
+              print("2. Add a new videos")
+              print("3. Update a videos")
+              print("4. Delete a videos")
+              print("5. Exit the app")
+              choice = input("Enter your choice: ")
+              if choice == "1":
+               list_videos()
+
+              elif choice == "2":
+               name = input("Enter the video name: ")       
+               time = input("Enter the video time: ") 
+               add_video(name,time)
+
+              elif choice == "3":
+               video_id = input("Enter the video id: ")
+               name = input("Enter the updated video name: ")       
+               time = input("Enter the updated video time: ") 
+               update_video(video_id,name,time)
+
+              elif choice == "4":
+               video_id = input("Enter the video id: ")
+               delete_video(video_id)                      
+
+              elif choice == "5":
+               break
+
+              else:
+                 print("Invalid choice")
+
+
+
+
+if __name__ == "__main__":
+       main()              
